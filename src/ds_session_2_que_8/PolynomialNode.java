af1f6fffdc62dd2c8e2ffd6d87f2ea8a3c9f8c29
popla <komalkhatri03021996@gmail.com>
2018-08-07 09:10:08 +0530

    polynimial

--- src/ds_session_2_que_8/PolynomialNode.java
+++ src/ds_session_2_que_8/PolynomialNode.java
@@ -1,10 +1,10 @@
 package ds_session_2_que_8;
 
 
-public class PolynomialList {
+public class PolynomialNode {
 	private int coefficient;
-	private VariableList firstTermOfVariableList;
-	private PolynomialList nextPolynomialList;
+	private VariableNode variableList;
+	private PolynomialNode nextPolynomialList;
 	
 	/**
 	 * @param  coefficient
@@ -12,16 +12,25 @@ public class PolynomialList {
 	 * Where first Column represent Index of variable and 
 	 * Second Column represent Power of variable
 	 */
-	PolynomialList(int coefficient, int variable[][]){
+	PolynomialNode(int coefficient, VariableNode variableList){
 		this.coefficient = coefficient;
-		if(variable.length != 0) {
+		this.variableList=variableList;
+		/*if(variable.length != 0) {
 			this.firstTermOfVariableList = new VariableList(variable[0][0], variable[0][1]);
 			VariableList variableList = this.firstTermOfVariableList;
 			for(int i=1; i<variable.length; i++) {
 				variableList.setNextVariableList( new VariableList(variable[i][0], variable[i][1]));
 				variableList = variableList.getNextVariableList();
-			}
-			this.nextPolynomialList = null;
-		}
+			}*/
+			this.setNextPolynomialList(null);
+	
+	}
+
+	public PolynomialNode getNextPolynomialList() {
+		return nextPolynomialList;
+	}
+
+	public void setNextPolynomialList(PolynomialNode nextPolynomialList) {
+		this.nextPolynomialList = nextPolynomialList;
 	}
 }
